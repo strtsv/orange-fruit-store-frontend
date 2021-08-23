e5497aeab34e4676b127cb04d794e360ab3687b1
jsusilo <enr@bk.ru>
2021-08-24 00:09:39 +0500

    Gallery Home

---
+++ src/components/Home/OurGallery.js
@@ -0,0 +1,272 @@
+import React from "react";
+
+import i1 from "../../assets/images/masonry-fullwidth-gallery-1-480x314.jpg";
+import i2 from "../../assets/images/masonry-fullwidth-gallery-1-480x314.jpg";
+import i3 from "../../assets/images/masonry-fullwidth-gallery-2-480x314.jpg";
+import i4 from "../../assets/images/masonry-fullwidth-gallery-2-480x314.jpg";
+import i5 from "../../assets/images/masonry-fullwidth-gallery-3-480x628.jpg";
+import i6 from "../../assets/images/masonry-fullwidth-gallery-3-480x628.jpg";
+import i7 from "../../assets/images/masonry-fullwidth-gallery-4-480x314.jpg";
+import i8 from "../../assets/images/masonry-fullwidth-gallery-4-480x314.jpg";
+import i9 from "../../assets/images/masonry-fullwidth-gallery-5-480x314.jpg";
+import i10 from "../../assets/images/masonry-fullwidth-gallery-5-480x314.jpg";
+import i11 from "../../assets/images/masonry-fullwidth-gallery-6-480x314.jpg";
+import i12 from "../../assets/images/masonry-fullwidth-gallery-6-480x314.jpg";
+import i13 from "../../assets/images/masonry-fullwidth-gallery-7-480x314.jpg";
+import i14 from "../../assets/images/masonry-fullwidth-gallery-7-480x314.jpg";
+
+class OurGallery extends React.Component {
+  render() {
+    return (
+      <section className="section section-xxl bg-image-1">
+        <div className="container-fluid isotope-wrap isotope-custom-2">
+          <h2 className="title-style-3 wow fadeScale">Featured offers</h2>
+          <p className="wow fadeInUp">
+            Take a look at organic fruits that we offer on a regular basis.
+          </p>
+          <div className="row row-30 isotope" data-lightgallery="group">
+            <div className="col-sm-6 col-md-4 col-xl-3 isotope-item">
+              {}
+              <article className="thumbnail-classic block-1">
+                <div className="thumbnail-classic-figure">
+                  <img src={i1} alt width={480} height={314} />
+                </div>
+                <div className="thumbnail-classic-caption">
+                  <div>
+                    <h5 className="thumbnail-classic-title">
+                      <a href="single-product.html">Pears</a>
+                    </h5>
+                    <div className="thumbnail-classic-price">$16.99</div>
+                    <div className="thumbnail-classic-button-wrap">
+                      <div className="thumbnail-classic-button">
+                        <a
+                          className="button button-gray-6 button-zakaria fl-bigmug-line-search74"
+                          href="images/masonry-fullwidth-gallery-1-1200x800-original.jpg"
+                          data-lightgallery="item"
+                        >
+                          <img src={i2} alt width={480} height={314} />
+                        </a>
+                      </div>
+                      <div className="thumbnail-classic-button">
+                        <a
+                          className="button button-primary button-zakaria fl-bigmug-line-shopping202"
+                          href="#"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+            <div className="col-sm-6 col-md-4 col-xl-3 isotope-item">
+              {}
+              <article className="thumbnail-classic block-1">
+                <div className="thumbnail-classic-figure">
+                  <img src={i3} alt width={480} height={314} />
+                </div>
+                <div className="thumbnail-classic-caption">
+                  <div>
+                    <h5 className="thumbnail-classic-title">
+                      <a href="single-product.html">Strawberries</a>
+                    </h5>
+                    <div className="thumbnail-classic-price">$17.99</div>
+                    <div className="thumbnail-classic-button-wrap">
+                      <div className="thumbnail-classic-button">
+                        <a
+                          className="button button-gray-6 button-zakaria fl-bigmug-line-search74"
+                          href="images/masonry-fullwidth-gallery-2-1200x800-original.jpg"
+                          data-lightgallery="item"
+                        >
+                          <img src={i4} alt width={480} height={314} />
+                        </a>
+                      </div>
+                      <div className="thumbnail-classic-button">
+                        <a
+                          className="button button-primary button-zakaria fl-bigmug-line-shopping202"
+                          href="#"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+            <div className="col-sm-6 col-md-4 col-xl-3 isotope-item">
+              {}
+              <article className="thumbnail-classic block-1">
+                <div className="thumbnail-classic-figure">
+                  <img src={i5} alt width={480} height={628} />
+                </div>
+                <div className="thumbnail-classic-caption">
+                  <div>
+                    <h5 className="thumbnail-classic-title">
+                      <a href="single-product.html">Pineapples</a>
+                    </h5>
+                    <div className="thumbnail-classic-price">$12.99</div>
+                    <div className="thumbnail-classic-button-wrap">
+                      <div className="thumbnail-classic-button">
+                        <a
+                          className="button button-gray-6 button-zakaria fl-bigmug-line-search74"
+                          href="images/masonry-fullwidth-gallery-3-1200x800-original.jpg"
+                          data-lightgallery="item"
+                        >
+                          <img src={i6} alt width={480} height={628} />
+                        </a>
+                      </div>
+                      <div className="thumbnail-classic-button">
+                        <a
+                          className="button button-primary button-zakaria fl-bigmug-line-shopping202"
+                          href="#"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+            <div className="col-sm-6 col-md-4 col-xl-3 isotope-item">
+              {}
+              <article className="thumbnail-classic block-1">
+                <div className="thumbnail-classic-figure">
+                  <img src={i7} alt width={480} height={314} />
+                </div>
+                <div className="thumbnail-classic-caption">
+                  <div>
+                    <h5 className="thumbnail-classic-title">
+                      <a href="single-product.html">Galia melons</a>
+                    </h5>
+                    <div className="thumbnail-classic-price">$12.99</div>
+                    <div className="thumbnail-classic-button-wrap">
+                      <div className="thumbnail-classic-button">
+                        <a
+                          className="button button-gray-6 button-zakaria fl-bigmug-line-search74"
+                          href="images/masonry-fullwidth-gallery-4-1200x800-original.jpg"
+                          data-lightgallery="item"
+                        >
+                          <img src={i8} alt width={480} height={314} />
+                        </a>
+                      </div>
+                      <div className="thumbnail-classic-button">
+                        <a
+                          className="button button-primary button-zakaria fl-bigmug-line-shopping202"
+                          href="#"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+            <div className="col-sm-6 col-md-4 col-xl-3 isotope-item">
+              {}
+              <article className="thumbnail-classic block-1">
+                <div className="thumbnail-classic-figure">
+                  <img src={i9} alt width={480} height={314} />
+                </div>
+                <div className="thumbnail-classic-caption">
+                  <div>
+                    <h5 className="thumbnail-classic-title">
+                      <a href="single-product.html">Melons</a>
+                    </h5>
+                    <div className="thumbnail-classic-price">$11.99</div>
+                    <div className="thumbnail-classic-button-wrap">
+                      <div className="thumbnail-classic-button">
+                        <a
+                          className="button button-gray-6 button-zakaria fl-bigmug-line-search74"
+                          href="images/masonry-fullwidth-gallery-5-1200x800-original.jpg"
+                          data-lightgallery="item"
+                        >
+                          <img src={i10} alt width={480} height={314} />
+                        </a>
+                      </div>
+                      <div className="thumbnail-classic-button">
+                        <a
+                          className="button button-primary button-zakaria fl-bigmug-line-shopping202"
+                          href="#"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+            <div className="col-sm-6 col-md-4 col-xl-3 isotope-item">
+              {}
+              <article className="thumbnail-classic block-1">
+                <div className="thumbnail-classic-figure">
+                  <img src={i11} alt width={480} height={314} />
+                </div>
+                <div className="thumbnail-classic-caption">
+                  <div>
+                    <h5 className="thumbnail-classic-title">
+                      <a href="single-product.html">Apples</a>
+                    </h5>
+                    <div className="thumbnail-classic-price">$9.99</div>
+                    <div className="thumbnail-classic-button-wrap">
+                      <div className="thumbnail-classic-button">
+                        <a
+                          className="button button-gray-6 button-zakaria fl-bigmug-line-search74"
+                          href="images/masonry-fullwidth-gallery-6-1200x800-original.jpg"
+                          data-lightgallery="item"
+                        >
+                          <img src={i12} alt width={480} height={314} />
+                        </a>
+                      </div>
+                      <div className="thumbnail-classic-button">
+                        <a
+                          className="button button-primary button-zakaria fl-bigmug-line-shopping202"
+                          href="#"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+            <div className="col-sm-6 col-md-4 col-xl-3 isotope-item">
+              {}
+              <article className="thumbnail-classic block-1">
+                <div className="thumbnail-classic-figure">
+                  <img src={i13} alt width={480} height={314} />
+                </div>
+                <div className="thumbnail-classic-caption">
+                  <div>
+                    <h5 className="thumbnail-classic-title">
+                      <a href="single-product.html">Lemons</a>
+                    </h5>
+                    <div className="thumbnail-classic-price">$18.99</div>
+                    <div className="thumbnail-classic-button-wrap">
+                      <div className="thumbnail-classic-button">
+                        <a
+                          className="button button-gray-6 button-zakaria fl-bigmug-line-search74"
+                          href="images/masonry-fullwidth-gallery-7-1200x800-original.jpg"
+                          data-lightgallery="item"
+                        >
+                          <img src={i14} alt width={480} height={314} />
+                        </a>
+                      </div>
+                      <div className="thumbnail-classic-button">
+                        <a
+                          className="button button-primary button-zakaria fl-bigmug-line-shopping202"
+                          href="#"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </div>
+          <a
+            className="button button-lg button-shadow-2 button-primary button-zakaria wow fadeInUp"
+            href="grid-shop.html"
+          >
+            View more
+          </a>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default OurGallery;
